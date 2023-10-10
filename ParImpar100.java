89d3addef28699fbe5cf59a4f79c490aef678f87
Sindo <126513686+Sindo12@users.noreply.github.com>
2023-10-10 13:39:49 +0200

    Add files via upload

---
+++ ParImpar100.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class ParImpar100{
+        public static void main(String[] args){
+            int i;
+            for(i = 0; i <=100; i++){
+            System.out.println(i);
+           }
+           if (i % 2 == 0)
+           System.out.println(i + "es impar");
+             else {
+            System.out.println(i + " es impar");
+            }
+        }
+    }
