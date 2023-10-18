29f5a0a267eb58151e5367b095703dbf5def2d35
Sindo <126513686+Sindo12@users.noreply.github.com>
2023-10-19 00:16:46 +0200

    Add files via upload

---
+++ HoMiSe.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+    public class HoMiSe{
+        public static void main(String[] args){
+            Scanner lector = new Scanner(System.in);
+            int horas, minutos, segundos;
+            System.out.println("Dime una cantidad de horas");
+                horas = lector.nextInt();
+            System.out.println("Dime una cantidad de minutos");
+                minutos = lector.nextInt();
+            System.out.println("Dime una cantidad de segundos");
+                segundos = lector.nextInt();
+            System.out.println(horas + ":" + minutos + ":" + segundos);
+
+
+            
+
+
+
+
+
+        }
+    }
