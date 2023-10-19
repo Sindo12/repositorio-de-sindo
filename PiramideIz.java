b9a194059978c3c210085d6cee6ed9ca18606b02
Sindo <126513686+Sindo12@users.noreply.github.com>
2023-10-19 19:49:50 +0200

    PiramideIz.java

--- PiramideIz.java
+++ PiramideIz.java
@@ -1,16 +1,10 @@
 public class PiramideIz {
     public static void main(String[] args) {
-        int num = 9;
-            //se ejecuta si i es menor o igual que n para ir aumentando los numeros de uno en uno
-            for (int i = 1; i <= num; i++) {
-            //imprimir espacios en blanco
-            for (int j = 1; j <= num - i; j++) 
-                System.out.print("  ");
-            //imprimir los numeros en orden
-            for (int k = 1; k <= i; k++) 
-                System.out.print(k + " ");
-            
-            System.out.println();
+        String s = "";
+        for (int i = 1; i <= 9; i++){
+            s+=i;
+            System.out.printf(format: "%9s\n" , s);
+        
         }
     }
 }
